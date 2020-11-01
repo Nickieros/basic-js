13485ccfcc92b9851dbdba9cdb3b6dc52a667817
Nickieros <>
2020-11-01 16:18:33 +0200

    feat: resolve what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!date) return 'Unable to determine the time of year!'
+  else if (!(date instanceof Date && 0 !== date - new Date())) throw new Error()
+
+  const month = date.getMonth();
+  return month === 11 || month < 2 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : 'autumn'
 };
