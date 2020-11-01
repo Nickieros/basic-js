ed4ddb717d66873e279f545079db7895ace9c19e
Nickieros <>
2020-11-01 19:45:28 +0200

    feat: resolve simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  data: [],
+  addLink: value => chainMaker.data.push(String(value)) && chainMaker,
+  reverseChain: () => chainMaker.data.reverse() && chainMaker,
+  removeLink(pos) {
+    if(isNaN(pos) || 0 > --pos || this.data.length < pos) { this.data = []; throw Error(); }
+    this.data.splice(pos, 1);
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const result = `( ${this.data.join(' )~~( ')} )`;
+    this.data = [];
+    return result;
   }
 };
 
