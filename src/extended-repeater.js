285d6fbb3c74fef97a7b88997f0ae7b06503d3b7
Nickieros <>
2020-11-01 22:04:05 +0200

    feat: resolve extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, o) {
+  let s = '';
+  if (!o.additionSeparator) o.additionSeparator = '|';
+  if (!o.separator) o.separator = '+';
+  if (undefined !== o.addition) s = (o.addition + '' + o.additionSeparator).repeat(o.additionRepeatTimes - 1) + o.addition;
+  return (str + s + o.separator).repeat(o.repeatTimes - 1) + str + s;
 };
   
