988dd1d473f3705d00fe8f0af74f6f037fa366a5
Nickieros <>
2020-11-01 15:25:30 +0200

    feat: resolve dream-team.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  return 'string' !== typeof sampleActivity && (MODERN_ACTIVITY < sampleActivity) && Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / 0.693 / HALF_LIFE_PERIOD);
+  return 'string' !== typeof sampleActivity
+      && MODERN_ACTIVITY < sampleActivity
+      && Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / 0.693 / HALF_LIFE_PERIOD);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members)
+      &&  members
+          .reduce((arSecretName, sMember) => {
+              if ('string' === typeof sMember) arSecretName.push(sMember.trim().substr(0,1).toUpperCase());
+              return arSecretName;
+            }, []
+          )
+          .sort()
+          .join('')
+      || false;
 };
